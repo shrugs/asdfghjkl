7adb79d352deb25ea80d87ef7500f97a270674b9
Matt Condon <mattgcondon@gmail.com>
2015-09-05 02:21:24 -0400

    Initial Commit

---
+++ asdfghjkl/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  asdfghjkl
+//
+//  Created by Matt Condon on 9/5/15.
+//  Copyright (c) 2015 mattc. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+  @IBOutlet weak var window: NSWindow!
+
+
+  func applicationDidFinishLaunching(aNotification: NSNotification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(aNotification: NSNotification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
